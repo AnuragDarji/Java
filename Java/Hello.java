5efb4312452a34bc4975bf0ff5aeb90d5df7f2ff
AnuragDarji <127482974+AnuragDarji@users.noreply.github.com>
2023-03-10 17:34:43 +0530

    Update Hello.java

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,5 +1,9 @@
+import java.util.*; 
 public class Hello{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+    public static void main(String[] args){ 
+        Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+        System.out.print("Enter your name: ");  
+        String name= sc.nextLine();
+        System.out.println("Hello "+name);
     }
 }
