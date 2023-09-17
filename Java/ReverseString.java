85c246bf29e474c0bde29cedc9a556cc2464a15a
AnuragDarji <127482974+AnuragDarji@users.noreply.github.com>
2023-09-17 12:28:45 +0530

    Update ReverseString.java

--- Java/ReverseString.java
+++ Java/ReverseString.java
@@ -15,3 +15,26 @@ class ReverseString {
         System.out.println("Reversed word: "+nstr);
     }
 }
+
+// ========== 2nd way ===========
+
+class ReverseString {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder(str);
+        for(int i=0; i<=sb.length()/2; i++){
+            int front = i;
+            int back = sb.length()-1-i;
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+        }
+        System.out.println("reversed string: "+sb);
+    }
+}
